e056bdbced1f69812dbbe565ad775243a772f7b2
xuxiaoyang <admin@aixieluo.com>
2021-02-24 16:49:28 +0800

    bug fix

--- application/extra/site.php
+++ application/extra/site.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'name' => 'My Website',
-    'beian' => '粤ICP备15000000号-1',
+    'name' => '小代发',
+    'beian' => '浙ICP备2020032103号-1',
     'cdnurl' => '',
     'version' => '1.0.1',
     'timezone' => 'Asia/Shanghai',
@@ -29,10 +29,10 @@ return [
     'mail_smtp_host' => 'smtp.qq.com',
     'mail_smtp_port' => '465',
     'mail_smtp_user' => '10000',
-    'mail_smtp_pass' => 'password',
+    'mail_smtp_pass' => '10000',
     'mail_verify_type' => '2',
-    'mail_from' => '10000@qq.com',
-    'meta_title' => 'title1',
-    'meta_description' => 'description',
-    'meta_keyword' => 'keyword',
+    'mail_from' => 'xiaodaifa@126.com',
+    'meta_title' => '',
+    'meta_description' => '小代发是全网专业的快递礼品代发平台，出售各种精致小礼品，AB单代发，快递代发，提供API对接服务，稳定的物流时效，完善的售后体系，实时订单全链路跟踪反馈，深受用户的好评！',
+    'meta_keyword' => '礼品代发,快递代发,ab单代发,ab单,真包裹网礼品代发,低价小礼品代发,信封礼品网,电商礼品网,礼品单网,信封代发网,快递单号代发,淘宝礼品代发平台,礼品网站,空包代发网站',
 ];
