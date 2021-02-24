3ff6b41d5a234b707e545fdc769555613c7ecd8c
xuxiaoyang <admin@aixieluo.com>
2021-02-24 16:23:02 +0800

    bug fix

--- application/index/controller/traits/OrderTrait.php
+++ application/index/controller/traits/OrderTrait.php
@@ -70,4 +70,25 @@ trait OrderTrait
         $order->save();
         return $order;
     }
+
+    public function arr_get($array, $key, $default = null)
+    {
+        if (! is_array($array)) {
+            return $default;
+        }
+        if (is_null($key)) {
+            return $array;
+        }
+        if (array_key_exists($key, $array)) {
+            return $array[$key];
+        }
+        foreach (explode('.', $key) as $segment) {
+            if (is_array($array) && array_key_exists($segment, $array)) {
+                $array = $array[$segment];
+            } else {
+                return $default;
+            }
+        }
+        return $array;
+    }
 }
